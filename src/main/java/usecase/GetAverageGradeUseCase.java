332f938a8b4ad48c3a3c3a0520638dfba100f6d9
KMdotcom <khizarmaraj01@gmail.com>
2025-10-06 14:27:51 -0400

    task 3 done

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -29,6 +29,15 @@ public final class GetAverageGradeUseCase {
         // TODO Task 3a: Complete the logic of calculating the average course grade for
         //              your team members. Hint: the getGrades method might be useful.
 
+        for (String member : team.getMembers()) {
+            for (Grade g : gradeDataBase.getGrades(member)) {
+                if (g.getCourse().equals(course)) {
+                    sum += g.getGrade();
+                    count++;
+                }
+            }
+        }
+
         if (count == 0) {
             return 0;
         }
