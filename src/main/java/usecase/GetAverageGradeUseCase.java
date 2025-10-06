afcea355d2ac58dc42ae041e0b0b0d821694cfcd
pyturtle <Nathan-nguyen@outlook.com>
2025-10-06 14:09:44 -0400

    Added task 3a and task 4

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -26,6 +26,11 @@ public final class GetAverageGradeUseCase {
         // TODO Task 3b: Go to the MongoGradeDataBase class and implement getMyTeam.
         final Team team = gradeDataBase.getMyTeam();
         // Call the API to get all the grades for the course for all your team members
+        String[] member_names = team.getMembers();
+        for(String name: member_names){
+            sum += gradeDataBase.getGrade(name, course).getGrade();
+            count++;
+        }
         // TODO Task 3a: Complete the logic of calculating the average course grade for
         //              your team members. Hint: the getGrades method might be useful.
 
